9bd765f61778e6a96ee9968fec09ce9d74695d5a
Oğuz Korkut <oguzkrkt@gmail.com>
2018-11-13 23:48:01 +0300

    Spring Boot + Spring Data JPA + Bootstrap

---
+++ springboot-springdatajpa-bootstrap/src/main/java/com/okorkut/springbootspringdatajpabootstrap/SpringbootSpringdatajpaBootstrapApplication.java
@@ -0,0 +1,12 @@
+package com.okorkut.springbootspringdatajpabootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringdatajpaBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringdatajpaBootstrapApplication.class, args);
+	}
+}
