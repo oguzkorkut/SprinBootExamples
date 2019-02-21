af386797b693020cd6f9604a93248f9bf57bda53
Oğuz Korkut <oguzkrkt@gmail.com>
2019-02-21 21:32:01 +0300

    internationalized çalışması yapıldı

--- restful-web-services/src/main/java/com/okorkut/rest/webservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/okorkut/rest/webservices/RestfulWebServicesApplication.java
@@ -1,7 +1,13 @@
 package com.okorkut.rest.webservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +16,27 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+//	@Bean
+//	public MessageSource messageSource() {
+//	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//	    messageSource.setBasename("classpath:messages");
+//	    messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+//	    return messageSource;
+//	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+		return messageSource;
+	}
 }
